d150c5b12e9105d8ade45b639dda5cbaa78d05d7
knowmaker <59835668+knowmaker@users.noreply.github.com>
2022-10-12 12:52:45 +0300

    new func

--- lab6_p3_func.rb
+++ lab6_p3_func.rb
@@ -5,14 +5,20 @@
 
 # class for calculating line length
 class Int
-
-  def self.intprg()
-    zn_a=0
-    zn_b=1
-    raz_h = (zn_b - zn_a).to_f / 10000
+  def self.intprg_block(zn_a, zn_b)
+    raz_h = (zn_b - zn_a).to_f / 10_000
     i_s = 0
-    (0..10000).each do |item|
+    10_001.times do |item|
       i_s += yield(zn_a + item * raz_h)
+    end
+    i_s * raz_h
+  end
+
+  def self.intprg_lambda(zn_a, zn_b, func)
+    raz_h = (zn_b - zn_a).to_f / 10_000
+    i_s = 0
+    10_001.times do |item|
+      i_s += func.call(zn_a + item * raz_h)
     end
     i_s * raz_h
   end
--- lab6_p3_main.rb
+++ lab6_p3_main.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 require_relative 'lab6_p3_func'
-
-#puts "Значение определенного интеграла : #{Int.intprg{Math.exp(x)/(x+1)}}"
-
+puts 'Введите 1 для функции exp(x)/(x+1) или 2 для x(x-1)'
+num = gets.to_i
+num = gets.to_i while num != 1 && num != 2
+if num == 1
+  puts "Значение определенного интеграла : #{Int.intprg_block(0, 1) { |x| Math.exp(x) / (x + 1) }}"
+  l_func = ->(x) { Math.exp(x) / (x + 1) }
+else
+  puts "Значение определенного интеграла : #{Int.intprg_block(0, 2) { |x| x * (x - 1) }}"
+  l_func = ->(x) { x * (x - 1) }
+end
+puts "Значение определенного интеграла : #{Int.intprg_lambda(0, 1, l_func)}"
